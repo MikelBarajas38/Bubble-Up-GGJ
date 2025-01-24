d58803679dec5127acebecdec852f6f06c5e0ac7
MikelBarajas38 <mikelbarajas388@gmail.com>
2025-01-24 10:06:50 -0600

    added initial bubble

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,19 +5,51 @@ extends CharacterBody2D
 @export var acceleration = 80
 @export var friction = 20
 @export var base_gravity = 1200
+@export var max_bubble_count = 3
 
 var jumping = false
 var dead = false
+var jump_count = 0
 
 var gravity = base_gravity
 var max_fall_speed = 1000
 
-var coyote_frames = 6
+var coyote_frames = 5
 var coyote = false
 var last_floor = false
 
+var bubble_count = 1
+var scale_map = [0, 0.5, 1.0, 1.5]
+var base_radius = 0
+
 func _ready():
+	base_radius = $CollisionShape2D.shape.radius
 	$CoyoteTimer.wait_time = coyote_frames / 60.0
+
+func handle_bubble_change():
+	$CollisionShape2D.shape.radius = base_radius * scale_map[bubble_count]
+
+func handle_death():
+	pass
+
+func handle_damage():
+	if bubble_count > 0:
+		pop_bubble()
+	else:
+		dead = true
+
+func pop_bubble():
+	bubble_count -= 1
+	if bubble_count < 1:
+		bubble_count = 1
+		dead = true
+	handle_bubble_change()
+
+func add_bubble():
+	bubble_count += 1
+	if bubble_count > max_bubble_count:
+		bubble_count = max_bubble_count
+	handle_bubble_change()
 
 func handle_input(delta):
 
@@ -27,9 +59,18 @@ func handle_input(delta):
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction * delta)
 
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
-		velocity.y = jump_speed
-		jumping = true
+	if Input.is_action_just_pressed("bubble"):
+		add_bubble()
+
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or coyote:
+			velocity.y = jump_speed
+			jump_count = jump_count + 1
+			jumping = true
+		elif jump_count < 1:
+			velocity.y = jump_speed
+			pop_bubble()
+			jump_count = jump_count + 1
 	
 	if jumping and !Input.is_action_pressed("jump"):
 		gravity = base_gravity * 2.5
@@ -39,9 +80,23 @@ func handle_input(delta):
 		else:
 			gravity = base_gravity
 
+	if is_on_floor() and jumping:
+		jumping = false
+		jump_count = 0
+	if !is_on_floor() and last_floor and !jumping:
+		coyote = true
+		$CoyoteTimer.start()
+
 func handle_animation():
 
-	if is_on_floor() and !jumping:
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+
+	if dead:
+		$AnimatedSprite2D.play("dead")
+	elif is_on_floor() and !jumping:
 		var moving = Input.get_axis("move_left", "move_right")
 		if moving:
 			$AnimatedSprite2D.play("run")
@@ -52,24 +107,13 @@ func handle_animation():
 			$AnimatedSprite2D.play("up")
 		else:
 			$AnimatedSprite2D.play("down")
-	
+
 func _physics_process(delta):
 
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, max_fall_speed)
 
 	handle_input(delta)
-
-	if is_on_floor() and jumping:
-		jumping = false
-	if !is_on_floor() and last_floor and !jumping:
-		coyote = true
-		$CoyoteTimer.start()
-
-	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
 
 	last_floor = is_on_floor()
 
