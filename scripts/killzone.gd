26dd43407b679955fcd5ff2ed214e7cfbd606a7f
Ricardo <rica-sal@hotmail.com>
2025-01-25 17:46:13 -0600

    change-level

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,8 +5,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	Engine.time_scale = 0.5
 	timer.start()
-	
-
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
---
+++ scripts/winzone.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var next_level: String
+
+func _on_body_entered(body: Node2D) -> void:
+	print("entre")
+	change_level()
+
+# Cambiar al siguiente nivel
+func change_level():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
