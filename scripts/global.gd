1929a12d91f810e82bf7449e9574a067c74e69c4
MikelBarajas38 <mikelbarajas388@gmail.com>
2025-01-26 02:43:08 -0600

    fixed respawn

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var bubble_count = 2
+var current_level = 1
--- scripts/winzone.gd
+++ scripts/winzone.gd
@@ -8,4 +8,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 # Cambiar al siguiente nivel
 func change_level():
+	Global.current_level = Global.current_level + 1
 	get_tree().change_scene_to_file(next_level)
+	
