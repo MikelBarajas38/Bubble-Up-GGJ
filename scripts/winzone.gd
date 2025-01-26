4c97906485ee213c72e9139639728be525072323
Montserrat Ximena Hernández Gallegos <gallegos230804@gmail.com>
2025-01-25 19:33:12 -0600

    Level transitions

--- scripts/winzone.gd
+++ scripts/winzone.gd
@@ -8,4 +8,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 # Cambiar al siguiente nivel
 func change_level():
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	get_tree().change_scene_to_file(next_level)
